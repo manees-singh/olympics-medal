d925a413d7c9d972713451c014a61802667fe1b1
manees <singhmanish5208@gmail.com>
2024-08-06 15:43:28 -0500

    added github actions

--- bot.py
+++ bot.py
@@ -7,7 +7,14 @@ bearer_token=r"AAAAAAAAAAAAAAAAAAAAABHpvAEAAAAABpYHWBipROsziSBBUicFKRelD68%3D4Nq
 access_token="1820868688284696576-9JEzBnn2gFy9xv8NSP83EsskiPELo3"
 access_token_secret="SHkSJ04s4PMKxVTog1eZBAliWtLSQq1xQ6HLs6OusatWh"
 
- 
+client=tweepy.Client(bearer_token,api_key,api_secret,access_token,access_token_secret)
+auth = tweepy.OAuth1UserHandler(
+    api_key, api_secret,
+    access_token, access_token_secret
+)
+
+
+api=tweepy.API(auth)
 #First tweet
 client.create_tweet(text="Hi!")
  
