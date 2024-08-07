00f26452df06e63c5161e5586aabe243160f942b
manees <singhmanish5208@gmail.com>
2024-08-07 15:36:00 -0500

    changed tweet text

--- bot.py
+++ bot.py
@@ -20,52 +20,24 @@ client = tweepy.Client(bearer_token=bearer_token,
 auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
 api = tweepy.API(auth)
 
-# Load medal data
-with open('medal_data.json', 'r') as f:
-    medal_data = json.load(f)
-
-# Select the top 10 countries
-top_countries = list(medal_data.items())[:10]  # Fetching top 10 instead of 20
-
-# Prepare the tweet text
-tweet_text = "#Paris2024\n"+ "#Olympics2024\n" +"#Olympics2024\n" + "#Olympics2024Paris\n" + "\n".join([f"{country_id}: {data[0]}" for country_id, data in top_countries])
-
-# Post the tweet with the text
-response = client.create_tweet(text=tweet_text)
 
 
 
 
-# import tweepy
-# import os
-# import json
-
-# api_key= os.getenv('API_KEY')
-# api_secret=os.getenv('API_SECRET')
-# bearer_token=  os.getenv('BEARER_TOKEN')
-# access_token= os.getenv('ACCESS_TOKEN')
-# access_token_secret= os.getenv('ACCESS_TOKEN_SECRET')
-
-# client=tweepy.Client(bearer_token,api_key,api_secret,access_token,access_token_secret)
-# auth = tweepy.OAuth1UserHandler(
-#     api_key, api_secret,
-#     access_token, access_token_secret
-# )
-
-
-# api=tweepy.API(auth)
-
-
+# # Load medal data
 # with open('medal_data.json', 'r') as f:
 #     medal_data = json.load(f)
 
 # # Select the top 10 countries
-# top_countries = list(medal_data.items())[:20]
+# top_countries = list(medal_data.items())[:15]  # Fetching top 15 s
 
 # # Prepare the tweet text
-# tweet_text = "Gold medal tally\n" + "\n".join([f"{country_id}: {data[0]}" for country_id, data in top_countries])
+# tweet_text = "#Paris2024\n"+ "#Olympics2024\n"  + "#Olympics2024Paris\n" +"Gold Medals tally\n"+ "\n".join([f"{country_id}: {data[0]}" for country_id, data in top_countries])
 
 # # Post the tweet with the text
 # response = client.create_tweet(text=tweet_text)
+
+
+
 
  
